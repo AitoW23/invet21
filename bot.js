0d39784dbb1c2139384ec72ad2644e150530ff68
GhostSlayer <aleksi.klen@gmail.com>
2019-12-12 20:33:47 +0200

    Fixed a bug

--- bot.js
+++ bot.js
@@ -43,7 +43,7 @@ const client = new Client({
         command.run(client, message, args);
       }
       catch(error) {
-        message.channel.send("An unknown error occured", ${err})
+        message.channel.send(`Something went wrong... ${err}`)
       }
   });
   
