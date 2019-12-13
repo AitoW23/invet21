795f87a6198f359c9ca3215a1be366626222df0b
GhostSlayer <51095261+GhostSlayer@users.noreply.github.com>
2019-12-13 20:04:25 +0200

    Update commands.js

--- handlers/commands.js
+++ handlers/commands.js
@@ -11,16 +11,6 @@ module.exports = (client) => {
     
         for (let file of commands) {
             let pull = require(`../commands/${dir}/${file}`);
-    
-            if (pull.name) {
-                client.commands.set(pull.name, pull);
-                table.addRow(file, '✅');
-            } else {
-                table.addRow(file, `❌  -> missing a help.name, or help.name is not a string.`);
-                continue;
-            }
-    
-            if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
         }
     });
     
