841bd6ca441a83a7a81e340ce7b8853775e2114e
GhostSlayer <aleksi.klen@gmail.com>
2019-12-13 15:14:09 +0200

    xd

--- shards.js
+++ shards.js
@@ -1,13 +1,13 @@
-const { ShardingManager } = require('discord.js');
+const { Client, ShardingManager } = require('discord.js');
+const PREFIX = process.env.PREFIX;
+
+const client = new Client({
+    disableEveryone: true
+  });
 const shard = new ShardingManager('./bot.js', {
   token: process.env.BOT_TOKEN,
   autoSpawn: true
 });
-
-shard.spawn(5);
-
-shard.on('launch', shard => console.log(`[SHARD] Shard ${shard.id}/5`));
-
 
 client.on('ready', () => { //Startup
     console.log(`Hi, ${client.user.username} is now online and connected to database!`);
@@ -16,3 +16,7 @@ client.on('ready', () => { //Startup
       type: 'playing'
     });
   });
+
+shard.spawn(5);
+
+shard.on('launch', shard => console.log(`[SHARD] Shard ${shard.id}/5`));
