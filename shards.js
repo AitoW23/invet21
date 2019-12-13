fc956351a18886c61fe58841bf50e8596839e284
GhostSlayer <aleksi.klen@gmail.com>
2019-12-13 14:52:19 +0200

    xd

--- shards.js
+++ shards.js
@@ -4,6 +4,6 @@ const shard = new ShardingManager('./bot.js', {
   autoSpawn: true
 });
 
-shard.spawn(2);
+shard.spawn(5);
 
 shard.on('launch', shard => console.log(`[SHARD] Shard ${shard.id}/${shard.totalShards}`));
