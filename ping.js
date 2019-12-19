957647619df0bf70eea1a6becb0160dacbbe220a
GhostSlayer <51095261+GhostSlayer@users.noreply.github.com>
2019-12-19 12:51:59 +0200

    Update ping.js

--- ping.js
+++ ping.js
@@ -1,6 +1,10 @@
+const { Client, Collection, Discord } = require("discord.js");
+
 const http = require('http');
 const express = require('express');
 const app = express();
 app.get("/", (request, response) => {
   response.sendStatus(200);
 });
+
+client.login(process.env.BOT_TOKEN)
