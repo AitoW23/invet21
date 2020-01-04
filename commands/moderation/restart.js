0a8d119a7070894202461af7c7d27ac3b554a854
AitoW205 <59112797+AitoW205@users.noreply.github.com>
2020-01-04 18:34:23 +0200

    Update restart.js

--- commands/moderation/restart.js
+++ commands/moderation/restart.js
@@ -2,7 +2,7 @@ exports.run = async (client, message, args) => {
     let ownerID = "304264050647498772";
     if (message.author.id == ownerID) {
     client.destroy()
-    .then(() => client.login(''))
+    .then(() => client.login('NjM2NTE2MjEzMTgyMzAwMTcz.XhC-cg.9-Rr0XEosgBoUkkx5udi5jGVXmo'))
     .then(() => message.channel.send(`The bot was successfully restarted!`));
     }
     else {
